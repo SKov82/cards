39afa1d62ec7dd1b1e079a579e3112b9a979ca93
skov82 <skov82@ya.ru>
2022-07-19 18:52:10 +0300

    add SuperComponents (input, button, checkbox)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import SuperComponent from './components/SuperComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Anki Cards</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Anki Cards</h1>
+            <SuperComponent />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/SuperComponent.tsx
@@ -0,0 +1,70 @@
+import React, {ChangeEvent, useState} from 'react'
+import SuperInputText from './ui/SuperInputText'
+import SuperButton from './ui/SuperButton'
+import SuperCheckbox from './ui/SuperCheckbox'
+import s from './SuperComponent.module.css'
+
+function HW4() {
+    const [text, setText] = useState<string>('')
+    const error = text ? '' : 'error'
+
+    const showAlert = () => {
+        if (error) {
+            alert('введите текст...')
+        } else {
+            alert(text) // если нет ошибки показать текст
+        }
+    }
+
+    const [checked, setChecked] = useState<boolean>(false)
+    const testOnChange = (e: ChangeEvent<HTMLInputElement>) => setChecked(e.currentTarget.checked)
+
+    return (
+        <div>
+            <div className={s.column}>
+                <SuperInputText
+                    value={text}
+                    onChangeText={setText}
+                    onEnter={showAlert}
+                    error={error}
+                    // spanClassName={s.testSpanError}
+                />
+
+                <SuperInputText
+                    className={s.blue} // проверьте, рабоет ли смешивание классов
+                />
+
+                {/*----------------------------------------------------*/}
+
+                <SuperButton>
+                    default
+                </SuperButton>
+
+                <SuperButton
+                    red // пропсу с булевым значением не обязательно указывать true
+                    onClick={showAlert}
+                >
+                    delete {/*// название кнопки попадёт в children*/}
+                </SuperButton>
+
+                <SuperButton disabled>
+                    disabled
+                </SuperButton>
+
+                {/*----------------------------------------------------*/}
+
+                <SuperCheckbox
+                    checked={checked}
+                    onChangeChecked={setChecked}
+                >
+                    some text {/*// этот текст попадёт в children*/}
+                </SuperCheckbox>
+
+                {/*// onChange тоже должен работать*/}
+                <SuperCheckbox checked={checked} onChange={testOnChange}/>
+            </div>
+        </div>
+    )
+}
+
+export default HW4
